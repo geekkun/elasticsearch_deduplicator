450ac85c6ef1d8f9643d7c17f03206f4345ef2cb
gkkn <geekkun@users.noreply.github.com>
2019-05-16 20:15:54 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(["localhost:9200"]) #replace host and port with your data
+
+# terms_size and top_hits_size can be up to 100; higher values lead to Read timed out exception
+# index.max_inner_result_window should be changed to increase size of top_hits_size, otherwise 100 is max for top_hits_size
+def find_docs(index, doc_type, terms_size, top_hits_size):
+  result = es.search(
+    index=index, 
+    doc_type=doc_type, 
+    body={
+    "size": 0,
+    "aggs": {
+      "duplicateCount": {
+        "terms": {
+          "field": "created",
+          "min_doc_count": 2,
+          "size": terms_size
+        },
+        "aggs": {
+          "duplicateDocuments": {
+            "top_hits": {
+              "size": top_hits_size
+            }}
+          }
+        }
+      }
+     }
+   )['aggregations']['duplicateCount']['buckets']
+  return result
+  
+def delete_duplicates(index, doc_type, result):
+  for res in result:
+    keys = res['duplicateDocuments']['hits']['hits']
+    for key_id in keys[1:]:
+        print(es.delete(index=index, doc_type=doc_type, id=key_id['_id']))
