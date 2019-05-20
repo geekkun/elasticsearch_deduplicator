cae0d8fa1a1493541b958b57da3e688edc7a8a1a
gkkn <geekkun@users.noreply.github.com>
2019-05-20 13:36:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch(["localhost:9200"]) #replace host and port with your data
+host = "127.0.0.1"
+port = 9200
+timeout = 1000
+index = "index"
+doc_type = "type"
+size = 1000
+
+es = Elasticsearch(
+    [
+        {
+            'host': host,
+            'port': port
+        }
+    ],
+    timeout=timeout
+)
 
 # terms_size and top_hits_size can be up to 100; higher values lead to Read timed out exception
 # index.max_inner_result_window should be changed to increase size of top_hits_size, otherwise 100 is max for top_hits_size
 # if 100 for terms_size and top_hits_size fails, try to decrease number a bit (e.g. 99 for both can work, depends on your server)
-def find_docs(index, doc_type, terms_size, top_hits_size):
+def get_docs(index, doc_type, terms_size, top_hits_size):
   result = es.search(
     index=index, 
     doc_type=doc_type, 
